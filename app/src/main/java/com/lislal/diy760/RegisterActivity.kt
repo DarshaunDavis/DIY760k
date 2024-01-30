7d65418655ec05407ef4cd63e5efae965a74310f
Darshaun Davis <34321400+DarshaunDavis@users.noreply.github.com>
2024-01-30 09:49:28 -0500

    -Add functionality to RegisterActivity and activity_register.xml -Reduce size of logo in activity_register.xml and activity_login.xml -Set all activities to portrait view in AndroidManifest.xml

--- app/src/main/java/com/lislal/diy760/RegisterActivity.kt
+++ app/src/main/java/com/lislal/diy760/RegisterActivity.kt
@@ -1,11 +1,38 @@
 package com.lislal.diy760
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class RegisterActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val firstNameEditText = findViewById<TextInputEditText>(R.id.firstNameEditText)
+        val emailEditText = findViewById<TextInputEditText>(R.id.emailEditText)
+        val passwordEditText = findViewById<TextInputEditText>(R.id.passwordEditText)
+        val confirmPasswordEditText = findViewById<TextInputEditText>(R.id.confirmPasswordEditText)
+
+        // Initialize the "Register" button and set its click listener
+        val registerButton = findViewById<Button>(R.id.submitRegisterButton)
+        registerButton.setOnClickListener {
+            // Here, you can add validation logic for registration fields
+            // For now, let's just navigate to MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Initialize the "Already Registered" text view and set its click listener
+        val alreadyRegisteredTextView = findViewById<TextView>(R.id.alreadyRegisteredTextView)
+        alreadyRegisteredTextView.setOnClickListener {
+            // Navigate to LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
