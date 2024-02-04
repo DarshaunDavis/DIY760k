bd892dcb2ee909e4a77e6c991e78adf69eb79a4d
Darshaun Davis <34321400+DarshaunDavis@users.noreply.github.com>
2024-02-04 02:54:53 -0500

    This commit enhances the MainFragment with UI and UX improvements for better navigation and user engagement. Key updates include:
    - Added swipe animations to arrow images, guiding users through app navigation.
    - Restructured layout with separate TextViews for clear content organization.
    - Refined app instructions for intuitive access to Guided Repair Journey and Self-Repair Portal.
    - Updated MainFragment.kt to integrate animations, boosting visual interaction cues.
    
    These changes aim to make DIY 760 more user-friendly, ensuring an intuitive and engaging experience.

--- app/src/main/java/com/lislal/diy760/MainFragment.kt
+++ app/src/main/java/com/lislal/diy760/MainFragment.kt
@@ -1,10 +1,13 @@
 package com.lislal.diy760
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
+import android.widget.ImageView
+import com.lislal.diy760.R
 
 class MainFragment : Fragment() {
 
@@ -14,5 +17,21 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Find the ImageView by its ID
+        val arrowLeft = view.findViewById<ImageView>(R.id.arrow_left)
+        val arrowRight = view.findViewById<ImageView>(R.id.arrow_right)
+
+        // Load the animations
+        val swipeLeftAnimation = AnimationUtils.loadAnimation(context, R.anim.anim_swipe_left)
+        val swipeRightAnimation = AnimationUtils.loadAnimation(context, R.anim.anim_swipe_right)
+
+        // Start the animations
+        arrowLeft?.startAnimation(swipeLeftAnimation)
+        arrowRight?.startAnimation(swipeRightAnimation)
     }
 }
